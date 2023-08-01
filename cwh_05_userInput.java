03dee54e35da8619fc56371997109cc847e25182
ShahabUddin <s4shahab@gmail.com>
2023-08-02 01:57:31 +0200

    user-input

---
+++ cwh_05_userInput.java
@@ -0,0 +1,23 @@
+//System.in means taking input from keyboard
+import java.util.Scanner;
+
+public class cwh_05_userInput {
+    public static void main(String[] args) {
+        System.out.println("Taking user input");
+        Scanner sc=new Scanner(System.in);
+//
+
+        boolean b1=sc.hasNextInt();
+        System.out.println(b1);
+
+//        System.out.println("Enter number 1 :");
+//        int a=sc.nextInt();
+//        float a=sc.nextFloat();
+//        System.out.println("Enter number 2 :");
+//        int b=sc.nextInt();
+//        float b=sc.nextFloat();
+//        int sum=a+b;
+//        System.out.println("The sum of a and b is : "+sum);
+
+    }
+}

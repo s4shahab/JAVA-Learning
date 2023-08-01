1f3ca979b0aa9d3422a1276dbbb789eb715d950f
ShahabUddin <s4shahab@gmail.com>
2023-08-02 01:56:37 +0200

    literals

---
+++ cwh_04_literls.java
@@ -0,0 +1,12 @@
+public class cwh_04_literls {
+    public static void main(String[] args) {
+        byte age=34;
+        System.out.println(age);
+        char ch= 'A';
+        System.out.println(ch);
+        float f1=4.53f;
+        double d1=4.533d;
+        System.out.println(f1);
+
+    }
+}

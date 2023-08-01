4e8a732c83f2a83a2361f5a8742a24cccb37a363
ShahabUddin <s4shahab@gmail.com>
2023-08-02 01:54:22 +0200

    java-intro

---
+++ Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World!");
+        int num1=5;
+        int num2=4;
+        int num3=8;
+         int sum=num1+num2+num3;
+        System.out.printf("The total is :"+sum);
+        System.out.println("new line");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+       
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+
+    }
+}

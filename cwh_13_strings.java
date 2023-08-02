a06bfe8d9fbce10a7271cd576ce48650db6003fd
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:06:01 +0200

    Strings

---
+++ cwh_13_strings.java
@@ -0,0 +1,11 @@
+public class cwh_13_strings {
+    public static void main(String[] args) {
+//        String name=new String("Harry");
+//        String name="Harrys";
+//        System.out.println(name);
+
+        int a=6;
+        float b=6.4455f;
+        System.out.printf("The value of a is %d and value of b is %f",a,b);
+    }
+}

a2dc396478d6326d2d6e230a8615b029f477dde2
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:00:31 +0200

    practice-set

---
+++ cwh_07_PS.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class cwh_07_PS {
+    public static void main(String[] args) {
+
+//        Question 01
+
+//        int a=5;
+//        int b=7;
+//        int c=8;
+//
+//        int sum=a+b+c;
+//        System.out.println("Sum of all number is :" +sum);
+
+//        Question 02
+
+//        int sub1=45;
+//        int sub2=95;
+//        int sub3=48;
+//        float sum=sub1+sub2+sub3;
+//        float cgpa=sum/30;
+//        System.out.println("The CGPA is :"+cgpa);
+
+
+//        Question 03
+
+        Scanner sc=new Scanner(System.in);
+//        String name=sc.next();
+//        System.out.println("Hello :" +name+ " have a good day ");
+
+
+//        Question 04
+
+//        System.out.println("Enter number in KM :");
+//        float valueInKM=sc.nextFloat();
+//        double InMile=valueInKM*(0.621371);
+//        System.out.println("The value in Mile is : "+InMile);
+
+//        Question 05
+
+        System.out.println("Enter your number :");
+        System.out.println(sc.hasNextInt());
+
+
+
+    }
+}

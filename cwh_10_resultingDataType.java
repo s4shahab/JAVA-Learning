a6dba2b22809e01cd0968bef90e9a1734e68697a
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:03:09 +0200

    resultingDataType

---
+++ cwh_10_resultingDataType.java
@@ -0,0 +1,37 @@
+public class cwh_10_resultingDataType {
+    public static void main(String[] args) {
+        int a=455+5;
+        float b=6.6f+4;
+
+        System.out.println(b);
+//        increment and decrement operator
+//        int x=45;
+//        int y=x++ , first y is assigned x then incremented
+
+//        int g=4;
+//        int h=++g; first g is incremented then assigned to h
+
+        int i=56;
+        System.out.println("value of i is :"+ i++);
+        System.out.println(i);
+        System.out.println("value of i is :"+ i++);
+        System.out.println(i);
+
+//
+
+        int k=4;
+        int d=++k*8;
+        System.out.println("the calue of d is :"+d);
+
+//
+
+        char as='B';
+
+        as++;
+        System.out.println("the character is :"+as);
+
+
+
+
+    }
+}

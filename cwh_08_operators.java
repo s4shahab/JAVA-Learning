70d82bdd73a32462c97dce2594261c55da8fe3e2
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:01:29 +0200

    operators-in-java

---
+++ cwh_08_operators.java
@@ -0,0 +1,21 @@
+public class cwh_08_operators {
+    public static void main(String[] args) {
+        int a=4;
+        a+=78;
+        int c=12;
+        int d=c%7; //modulo operator
+        System.out.println(a);
+        System.out.println(6==8);
+        System.out.println(64>5 && 64>12);
+        System.out.println(8!=5);
+
+        System.out.println(2|3);
+
+
+//        2=10
+//        3=11
+
+//        ------
+//       AND  10
+    }
+}

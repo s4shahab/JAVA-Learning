e965cc3c21b7731c5275b79a97a99a4c2426a1a2
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:08:08 +0200

    conditionals

---
+++ cwh_16_ifElse.java
@@ -0,0 +1,11 @@
+public class cwh_16_ifElse {
+    public static void main(String[] args) {
+        int age=20;
+        if (age>18){
+            System.out.println("Yes can drive");
+        }
+        else {
+            System.out.println("not yet drive");
+        }
+    }
+}

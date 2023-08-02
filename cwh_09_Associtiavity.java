1809c0889900a246984bdd52f441660f39e46789
ShahabUddin <s4shahab@gmail.com>
2023-08-02 02:02:23 +0200

    associtivity

---
+++ cwh_09_Associtiavity.java
@@ -0,0 +1,16 @@
+public class cwh_09_Associtiavity {
+    public static void main(String[] args) {
+//        precedence and associtivity
+        int a=15*2-34/2;
+        System.out.println(a);
+
+        int b=6/5-34*2;
+        System.out.println(b);
+
+        int c=60/5-34*2;
+        System.out.println(c);
+
+
+    }
+
+}

6cb5c56271d48355effea2e113e6816cfd6549c2
ShahabUddin <s4shahab@gmail.com>
2023-08-07 19:30:10 +0200

    updating this-super-keyword

--- cwh_47_this_super.java
+++ cwh_47_this_super.java
@@ -1,34 +1,47 @@
 class Ekclass{
 
     Ekclass(int a){
+        System.out.println("step 1");
         this.a=a;
+        System.out.println("step 2");
 
     }
     int a;
 
     public int getA() {
+        System.out.println("step 3");
         return a;
+
     }
 
     public int returnone(){
+        System.out.println("step 4");
         return 1;
     }
 }
 
 class DClass extends Ekclass{
     DClass(int z){
+
         super(z);
+        System.out.println("step 5");
         System.out.println("I am constructor of DClass");
     }
 }
+
+//before super not taking any sout
 
 
 public class cwh_47_this_super {
 
     public static void main(String[] args) {
 
+        System.out.println("step 6");
+
         Ekclass ekc=new Ekclass(5);
+        System.out.println("step 7");
         DClass dkc=new DClass(45);
-        System.out.println(ekc.getA());
+        System.out.println("step 8");
+//        System.out.println(ekc.getA());
 
     }}
---
+++ cwh_49_dynamic_method_dispatch.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class cwh_49_dynamic_method_dispatch {
+}

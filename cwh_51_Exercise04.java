d3c07955a13e67a62a37bf7a0626e2300e0ec409
ShahabUddin <s4shahab@gmail.com>
2023-08-08 17:13:05 +0200

    Exercise 4 online library 1st

--- cwh_51_Exercise04.java
+++ cwh_51_Exercise04.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class cwh_51_Exercise04 {
+import java.util.ArrayList;
+import java.util.Scanner;
+class Library{
+
+    Library(){
+
+    }
+    public void addBooks(String abook){
+        availableBooks.add(abook);
+        System.out.println("The available books are :" +availableBooks);
+
+
+    }
+
+    public void issueBook(String ibook){
+        availableBooks.remove(ibook);
+        System.out.println("After issuing book %\s the remaining books are:  "+availableBooks);
+
+    }
+
+    public void returnBook(){
+
+    }
+
+    public void showAvailableBooks(){
+
+    }
+
+//    String [] availableBooks={"Physics","Chemistry","Mathematics","English","Urdu"};
+//    String [] issuedBooks={};
+
+    ArrayList<String> availableBooks=new ArrayList<String>();
+
+
+
+
+
+
+}
+
+
+
+public class cwh_51_Exercise04 {
+    public static void main(String[] args) {
+//        Implement a library using Java class library
+//        Methods: addBook, issueBook, returnBook,showAvailableBooks.
+//        Properties: Array to store the available Books
+//        Array to store the issued Books
+
+        Library lib=new Library();
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Do you want to add books to Library : ");
+        System.out.println("Enter Book name to add in Library: ");
+        String abook=sc.next();
+        lib.addBooks(abook);
+
+        System.out.println("Enter name of book to be issue: ");
+        String ibook=sc.next();
+        lib.issueBook(ibook);
+
+
+
+
+    }
 }
---
+++ cwh_52_Practice.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class cwh_52_Practice {
+}

7d821c89c4c60ca0287b9e843049e2740ecdf33c
ShahabUddin <s4shahab@gmail.com>
2023-08-12 23:22:53 +0200

    Threading_runnable_interface

---
+++ cwh_71_Threading_runnable_interface.java
@@ -0,0 +1,191 @@
+class MyThreadRunnable_1 implements Runnable{
+    public void run(){
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+        System.out.println("I am a thread 1");
+    }
+}
+
+class MyThreadRunnable_2 implements Runnable{
+    public void run(){
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+        System.out.println("I am a thread 2");
+    }
+}
+
+public class cwh_71_Threading_runnable_interface {
+    public static void main(String[] args) {
+
+        MyThreadRunnable_1 bullet1=new MyThreadRunnable_1();
+        Thread gun1=new Thread(bullet1);
+
+
+        MyThreadRunnable_2 bulet2=new MyThreadRunnable_2();
+        Thread gun2=new Thread(bulet2);
+
+//        we can not directly write start method on above objects.
+
+        gun1.start();
+        gun2.start();
+
+    }
+}

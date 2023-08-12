4d346de3481def810c30bf74748fe505e97c4998
ShahabUddin <s4shahab@gmail.com>
2023-08-12 20:54:59 +0200

    accessModifiers

--- cwh_66_accessModifiers.java
+++ cwh_66_accessModifiers.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class cwh_66_accessModifiers {
+class C1{
+    public int x=4;
+    private int y=6;
+    int z=65;
+    protected int a=25;
+
+    public void meth1(){
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+    }
+
+
+}
+
+public class cwh_66_accessModifiers {
+    public static void main(String[] args) {
+        C1 c=new C1();
+//        c.meth1();
+
+        System.out.println(c.x);
+//        System.out.println(c.y); //not access due to private private in same package
+        System.out.println(c.z);
+        System.out.println(c.a);
+    }
 }

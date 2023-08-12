b26091a852df3e4da890b17423fb336eee2ee6dd
ShahabUddin <s4shahab@gmail.com>
2023-08-12 20:53:05 +0200

    Packages

---
+++ cwh_65_packages.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+//import java.util.*;
+
+import java.util.Scanner;
+
+public class cwh_65_packages {
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner("Scanner");
+        java.util.Scanner sc=new java.util.Scanner(System.in);  //alternate to import Scanner class
+
+        int a=sc.nextInt();
+        System.out.println("This is Scanner taking input :"+a);
+
+
+    }
+}

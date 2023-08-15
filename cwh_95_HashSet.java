e4def53a02318065b706e54d6bc6bf4f2b3e66ed
ShahabUddin <s4shahab@gmail.com>
2023-08-15 23:02:11 +0200

    HashSet

--- cwh_95_HashSet.java
+++ cwh_95_HashSet.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class cwh_95_HashSet {
+import java.util.HashSet;
+
+public class cwh_95_HashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> HS=new HashSet<>(6,0.5f);
+        HS.add(6);
+        HS.add(12);
+        HS.add(3);
+        HS.add(11);
+        HS.add(11);
+
+        System.out.println(HS);
+    }
 }

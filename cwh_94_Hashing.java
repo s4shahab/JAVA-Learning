479bf3249d7358d0e8007643983e4f7c90b06751
ShahabUddin <s4shahab@gmail.com>
2023-08-15 23:01:13 +0200

    Hashing

---
+++ cwh_94_Hashing.java
@@ -0,0 +1,22 @@
+public class cwh_94_Hashing {
+    public static void main(String[] args) {
+//        Hashing is a technique to convert rang eof key values pairs into range of indices.
+//        chaining technique
+//        f(x)=x%10
+//        let
+//        x=72-----at----->index--2
+//        x=63---------at----->index--3
+//        x=21---------at----->index--1
+
+//        Load factor---capacity of storage of hashtable
+
+//        classed used
+//        ------------------HashSet
+//        ------------------HashMap
+//        ------------------LinkedHashMap
+//        ------------------HashTable
+//
+    }
+}
+
+

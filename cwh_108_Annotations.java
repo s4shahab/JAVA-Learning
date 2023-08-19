025cdcb7e92f31baec94761f382c7d480c926730
ShahabUddin <s4shahab@gmail.com>
2023-08-20 01:42:54 +0200

    Annotations-Lambda_Expressions

--- cwh_108_Annotations.java
+++ cwh_108_Annotations.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class cwh_108_Annotations {
+
+
+@FunctionalInterface
+interface myFunctionalInteface{
+    void thisMethod();
+//    void thisMethod2();
+}
+
+class NewPhone extends Phone{
+    @Override
+    public void showTime(){
+        System.out.println("Time is 8PM");
+    }
+    @Deprecated
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
+public class cwh_108_Annotations {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        NewPhone phone = new NewPhone();
+        phone.showTime();
+        phone.sum(5, 6);
+    }
 }

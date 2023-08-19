ac7aa9041b5ab01d577549447d50dee8bc680b91
ShahabUddin <s4shahab@gmail.com>
2023-08-20 01:40:52 +0200

    java-doc

--- cwh_106_javaDoc.java
+++ cwh_106_javaDoc.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class cwh_106_javaDoc {
+/**
+ * <i>This is a simple documentation to show that HTML elements can be included in JavaDoc.</i>
+ * @see <a href="https://www.codewithharry.com/videos/java-tutorials-for-beginners-1">Best Java Course Available For <b>Free</b></a>
+ * */
+
+public class cwh_106_javaDoc {
+    /** This method is used to return the divison of two numbers
+     * @throws ArithmeticException if divided by 0
+     * @return Integer
+     * @param a First parameter -  Integer
+     * @param b Second parameter - Integer
+     * */
+    public int divison(int a, int b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
 }
+
+
